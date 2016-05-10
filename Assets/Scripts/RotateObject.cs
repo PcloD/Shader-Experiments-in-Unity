37e6577e22db89f69914cd0675d2585aa2310ad0
Brian Porter <brian.john.porter@gmail.com>
2016-05-10 01:23:05 -0400

    Watercolor Effect and 2nd Bottle mesh

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Vector3 t = transform.localEulerAngles;
+            t.x += 10 * Time.deltaTime;
+            transform.eulerAngles = t;
+        }
+    }
+}
